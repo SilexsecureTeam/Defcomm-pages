238cc717ffa952097d00610f54357f623809a5dc
Caleb <calebshokunbi619@gmail.com>
2025-06-13 16:38:52 +1200

    services

---
+++ src/pages/ServicesV2.jsx
@@ -0,0 +1,19 @@
+import Nav from "../components/landing/Header/Nav.jsx"
+import HomeService2 from "../components/landing/Content/HomeService2.jsx"
+import CoreValue2 from "../components/landing/Content/CoreValue2.jsx"
+import ResultsVideo from "../components/landing/Content/ResultsVideo.jsx"
+import OurProduct from "../components/landing/Content/OurProduct.jsx"
+import Footer from "../components/landing/Footer/Footer.jsx"
+import BackToTopButton from "../components/BackToTopButton.jsx"
+
+function Services2() {
+    return (
+        <div className="w-full mx-auto">
+            <HomeService2 />
+            <CoreValue2 />
+            {/* <OurProduct /> */}
+        </div>
+    )
+}
+
+export default Services2
