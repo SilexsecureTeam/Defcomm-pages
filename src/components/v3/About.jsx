2fb33133a972a21b89dfe7d4af54de6f3f0346b1
Adeemma02 <emm.bolu@gmail.com>
2025-07-30 23:02:53 +0100

    about hero section moblie

--- src/components/v3/About.jsx
+++ src/components/v3/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="bg-[#141413] inter pt-14">
-      <div className="min-h-screen flex items-center justify-center p-8">
+      <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 py-16">
         <div className="max-w-4xl mx-auto">
           {/* Main heading with bracket borders */}
           <motion.div
