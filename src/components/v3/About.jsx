8d2ecb9ff0b619b84346ae0e361311889a939c1c
Adeemma02 <emm.bolu@gmail.com>
2025-07-31 13:31:38 +0100

    font size

--- src/components/v3/About.jsx
+++ src/components/v3/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="bg-[#141413]">
-      <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 py-16">
+      <div className=" flex items-center justify-center px-4 sm:px-6 lg:px-8 py-12">
         <div className="max-w-4xl mx-auto">
           {/* Main heading with bracket borders */}
           <motion.div
