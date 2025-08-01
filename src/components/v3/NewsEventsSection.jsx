b33ceaf05357ac61d67da8b738d8daadd3d84d4a
Adeemma02 <emm.bolu@gmail.com>
2025-08-01 09:12:35 +0100

    margin buttom

--- src/components/v3/NewsEventsSection.jsx
+++ src/components/v3/NewsEventsSection.jsx
@@ -59,7 +59,7 @@ export default function NewsEventsSection() {
         />
 
         <motion.p
-          className="text-white text-xl font-medium leading-tight max-w-2xl"
+          className="text-white text-xl font-medium mb-16 leading-tight max-w-2xl"
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
