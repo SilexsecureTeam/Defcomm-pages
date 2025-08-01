de3725ae2b8c1f9dffe50988cbe541fdb4f30745
Adeemma02 <emm.bolu@gmail.com>
2025-08-01 22:57:49 +0100

    hardware

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import FallBack from "./components/Fallback";
 import Blog1Page from "./pages/v3/Blog1Page";
 import Blog2Page from "./pages/v3/Blog2Page";
 import Blog3Page from "./pages/v3/Blog3Page";
+import Hardware from "./pages/v3/Hardware";
 // import ScrollToTop from "./components/ScrollToTop";
 
 const MainLayout = lazy(() => import("./layout/MainLayout"));
@@ -40,6 +41,7 @@ const App = () => {
             <Route path="/launches-defcomm" element={<Blog1Page />} />
             <Route path="/defcomm-attend" element={<Blog2Page />} />
             <Route path="/defcomm-unveil" element={<Blog3Page />} />
+            <Route path="/hardware" element={<Hardware />} />
 
             <Route path="/bounty" element={<LiveFire />} />
             {/* Catch-all redirect */}
