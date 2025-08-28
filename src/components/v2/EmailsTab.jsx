f553ebebc81469313ae9204b3efaa2a926c5ce5d
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-08-28 21:45:06 +0100

    latest

--- src/components/v2/EmailsTab.jsx
+++ src/components/v2/EmailsTab.jsx
@@ -62,7 +62,7 @@ export default function EmailsTab() {
                   initial={{ opacity: 0, height: 0 }}
                   animate={{ opacity: 1, height: "auto" }}
                   exit={{ opacity: 0, height: 0 }}
-                  className="px-3 pb-3 text-gray-200 overflow-hidden"
+                  className="px-3 pb-3 text-sm text-gray-200 overflow-hidden"
                 >
                   {email.body}
                 </motion.div>
